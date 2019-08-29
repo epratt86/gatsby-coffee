64d97d73633215030824bd0b13d0fefbd453bfb4
Eric Pratt <ericpratt86@gmail.com>
2019-08-29 13:09:30 -0700

    Added background image component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Regular Joe's`,
+    title: `Coffee Time`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },

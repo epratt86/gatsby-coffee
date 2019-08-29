28055e4fafeafebac80ffcd4011261e23195ee70
Eric Pratt <ericpratt86@gmail.com>
2019-08-29 14:10:09 -0700

    about page

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="about us"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "slow-pour.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage

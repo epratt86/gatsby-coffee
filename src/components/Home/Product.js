b577a1b3e09b3d04cd27dbadc211b33ea97f27ce
Eric Pratt <ericpratt86@gmail.com>
2019-09-09 14:02:25 -0700

    snipcart setup

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -13,7 +13,14 @@ export default function Product({ product }) {
         <div className="card-body text-center">
           <h6>{product.title}</h6>
           <p>${product.price}</p>
-          <button className="btn btn-yellow mt-3 text-capitalize">
+          <button
+            className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+            data-item-id={product.id}
+            data-item-name={product.title}
+            data-item-price={product.price}
+            data-item-image={product.image.fluid.src}
+            data-item-url="https://coffee-time-app.netlify.com"
+          >
             add to cart
           </button>
         </div>
